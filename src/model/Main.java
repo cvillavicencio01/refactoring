9cc1e0e9634f9bd07464ce0cd4c7bcc3725e4de3
Carlos Villavicencio <cvillavicencio01@gmail.com>
2018-01-18 11:55:04 +0100

    main implementation

--- src/model/Main.java
+++ src/model/Main.java
@@ -3,8 +3,20 @@ package model;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Movie rogue = new Movie("Rogue One", Movie.NEW_RELEASE);
+		Movie reine = new Movie("Reine des neiges", Movie.CHILDRENS);
+		Movie star = new Movie("Star Wars III", Movie.REGULAR);
+		
+		Rental rental1 = new Rental(rogue, 5);
+		Rental rental2 = new Rental(reine, 7);
+		Rental rental3 = new Rental(star, 4);
+		
+		Customers customer = new Customers("Charlie");
+		customer.addRental(rental1);
+		customer.addRental(rental2);
+		customer.addRental(rental3);
+		
+		System.out.println(customer.statement());		
 	}
 
 }
