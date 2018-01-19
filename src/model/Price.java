70b19e47c28321e319e995952a5887bab7951fbb
Carlos Villavicencio <cvillavicencio01@gmail.com>
2018-01-19 18:13:19 +0100

    We provide the type code behavior in the price object. We use one abstract class and the subclasses

---
+++ src/model/Price.java
@@ -0,0 +1,32 @@
+package model;
+
+public abstract class Price {
+	abstract int getPriceCode();
+}
+
+class ChildrensPrice extends Price {
+
+	@Override
+	int getPriceCode() {
+		return Movie.CHILDRENS;
+	}
+
+}
+
+class NewReleasePrice extends Price {
+
+	@Override
+	int getPriceCode() {
+		return Movie.NEW_RELEASE;
+	}
+
+}
+
+class RegularPrice extends Price {
+
+	@Override
+	int getPriceCode() {
+		return Movie.REGULAR;
+	}
+
+}
